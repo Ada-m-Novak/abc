709927aa3dce77654269ab4b3b20cb36cbfccb3a
Eidam007 <simadam101@gmail.com>
2026-04-29 13:55:19 +0200

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -1,47 +1,72 @@
 #include "Account.h"
 
-int Account::nextNumber = 2000;
 int Account::totalAccounts = 0;
 
 Account::Account(int c, Client *o)
-    : number(nextNumber++), balance(c), interestRate(0.01), owner(o) {
-  totalAccounts++;
+{
+    number = c;
+    owner = o;
+    balance = 0;
+    interestRate = 0;
+    totalAccounts++;
 }
 
 Account::Account(int c, Client *o, double ir)
-    : number(nextNumber++), balance(c), interestRate(ir), owner(o) {
-  totalAccounts++;
+{
+    number = c;
+    owner = o;
+    balance = 0;
+    interestRate = ir;
+    totalAccounts++;
 }
 
-Account::~Account() { totalAccounts--; }
-
-int Account::GetNumber() const { return number; }
-
-double Account::GetBalance() const { return balance; }
-
-double Account::GetInterestRate() const { return interestRate; }
-
-Client *Account::GetOwner() const { return owner; }
-
-Client *Account::GetClient() const { return nullptr; }
-
-bool Account::CanWithdraw(double a) const { return a >= 0 && balance >= a; }
-
-void Account::Deposit(double a) {
-  if (a > 0) {
-    balance += a;
-  }
+Account::~Account()
+{
+    totalAccounts--;
 }
 
-bool Account::Withdraw(double a) {
-  if (!CanWithdraw(a)) {
+int Account::GetNumber() const
+{
+    return number;
+}
+
+Client *Account::GetOwner() const
+{
+    return owner;
+}
+
+double Account::GetBalance() const
+{
+    return balance;
+}
+
+double Account::GetInterestRate() const
+{
+    return interestRate;
+}
+
+void Account::Deposit(double a)
+{
+    if (a > 0)
+        balance += a;
+}
+
+bool Account::Withdraw(double a)
+{
+    if (CanWithdraw(a))
+    {
+        balance -= a;
+        return true;
+    }
     return false;
-  }
-
-  balance -= a;
-  return true;
 }
 
-void Account::AddInterest() { balance += balance * interestRate; }
+bool Account::CanWithdraw(double a) const
+{
+    return a >= 0 && balance >= a;
+}
 
-int Account::GetTotalAccounts() { return totalAccounts; }
+Client *Account::GetClient() const
+{
+    return nullptr;
+}
